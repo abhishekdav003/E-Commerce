abdbc118477081c30f3d9e4be1ccd5c893482d90
abhishekdav003 <abhishekdav003@gmail.com>
2025-02-12 10:34:42 +0530

    store modified

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './userSlice'
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
-  reducer: {
-    user : userReducer
-  },
-})
+  reducer: { user: persistedReducer },
+});
+
+export const persistor = persistStore(store);
